8b87075a0fe2ceb9af549f122bdde4889504bd2a
Malcon Augusto Souza de Miranda <malcon.miranda@nm.local>
2023-06-06 18:55:27 -0300

    refactor: add generic params to HttpPostClient

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -1,4 +1,4 @@
-export enum HttpStatusCode{
+export enum HttpStatusCode {
   ok = 200,
   noContent = 204,
   badRequest = 400,
@@ -6,7 +6,7 @@ export enum HttpStatusCode{
   notFound = 404,
   serverError = 500,
 }
-export type HttpResponse ={
-  statusCode: HttpStatusCode
-  body?: any
-}
+export type HttpResponse<T> = {
+  statusCode: HttpStatusCode;
+  body?: T;
+};
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,18 +1,31 @@
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { HttpPostClient } from '@/data/protocols/http/httpPostClient'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
+import { HttpPostClient } from "@/data/protocols/http/httpPostClient";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
+import { UnexpectedError } from "@/domain/errors/unexpected-error";
+import { AccountModel } from "@/domain/models/account-model";
+import { AuthenticationParams } from "@/domain/usecases/authentication";
 export class RemoteAuthentication {
-  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) {}
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<
+    AuthenticationParams,
+    AccountModel
+    >,
+  ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
-    const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
+  async auth(params: AuthenticationParams): Promise<void> {
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params,
+    });
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok:break
-      case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
-      default: throw new UnexpectedError()
+      case HttpStatusCode.ok:
+        break;
+      case HttpStatusCode.unathorized:
+        throw new InvalidCredentialsError();
+      default:
+        throw new UnexpectedError();
     }
   }
 }
